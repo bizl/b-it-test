9748066bf90d07eb6427cb19d1e511eb14e48191
DESKTOP-DM1RVNT\bongyu <bime123@gmail.com>
2023-02-13 00:18:33 +0000

    Domain project added

---
+++ CustomerManager/CustomerManager.Domain/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomerManager.Domain
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int? Age { get; set; }
+        public string? Address { get; set; }
+        public string? Reference { get; set; }
+        public string? Telephone { get; set; }
+
+    }
+}
