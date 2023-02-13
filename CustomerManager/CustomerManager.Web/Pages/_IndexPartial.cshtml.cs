3dbc718905125b4245a71cbc2bbd7b9427ae143e
DESKTOP-DM1RVNT\bongyu <bime123@gmail.com>
2023-02-13 02:28:48 +0000

    Css, js

---
+++ CustomerManager/CustomerManager.Web/Pages/_IndexPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CustomerManager.Web.Pages
+{
+    public class _IndexPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
